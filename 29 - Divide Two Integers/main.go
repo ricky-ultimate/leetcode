bd820aaac78cded4c49adf6658c50386d5c9a078
Ricky Ultimate <rickyogigi@gmail.com>
2024-11-21 00:15:53 +0100

    feat: Implement go solution for problem 29

---
+++ 29 - Divide Two Integers/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "math"
+
+func divide(dividend int, divisor int) int {
+	if dividend == math.MinInt32 && divisor == -1 {
+		return math.MaxInt32
+	}
+	return dividend / divisor
+}
